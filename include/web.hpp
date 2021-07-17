e707cc3f9207fb8401ca6236a72793ea141bde8e
skyhoffert <skwylarh@yahoo.com>
2021-07-17 14:02:42 -0400

    Wrote beginnings of insert, remove, and find.

--- include/web.hpp
+++ include/web.hpp
@@ -11,6 +11,8 @@ public:
     WebNode(int n_children);
     ~WebNode();
 
+    WebNode* FirstChildWithAtMost(int v);
+
 private:
 
     // TODO(sky): this should be templated!
@@ -29,6 +31,28 @@ public:
     Web(int n_children);
     ~Web();
 
+    // Adds a value to this web instance.
+    // TODO(sky): are duplicates allowed?
+    //
+    // @param v(int): value to be inserted.
+    // @return int: 0 on success, error code otherwise.
+    int Insert(int v);
+
+    // Removes a value from this web instance.
+    // 
+    // @param v(int): value to be removed.
+    // @return int: 0 on success, error code otherwise.
+    int Remove(int v);
+
+    // Finds a value in this web, if possible.
+    //
+    // @param v(int): value to be found.
+    // @return int: 0 on success, error code otherwise.
+    int Find(int v);
+    
+    // Returns the number of "nodes" within this web instance.
+    //
+    // @return int: number of nodes in this web.
     int Size();
 
 private:
--- src/web.cpp
+++ src/web.cpp
@@ -13,8 +13,6 @@
 // WebNode implementations.
 
 WebNode::WebNode(int n_children) {
-    printf("DEBUG:WebNode init started.\n");
-
     _n_children = n_children;
 
     _children = new WebNode*[_n_children];
@@ -23,13 +21,9 @@ WebNode::WebNode(int n_children) {
         printf("DEBUG:Creating child %d as nullptr.\n", i);
         _children[i] = nullptr;
     }
-
-    printf("DEBUG:WebNode init ended.\n");
 }
 
 WebNode::~WebNode() {
-    printf("DEBUG:WebNode destr started.\n");
-
     for (int i = 0; i < _n_children; ++i) {
         printf("DEBUG:Deleting child %d.\n", i);
         if (_children[i] != nullptr) {
@@ -37,8 +31,11 @@ WebNode::~WebNode() {
             _children[i] = nullptr;
         }
     }
+}
 
-    printf("DEBUG:WebNode destr ended.\n");
+WebNode* WebNode::FirstChildWithAtMost(int v) {
+    // TODO(sky): implement.
+    return nullptr;
 }
 
 // WebNode implementations.
@@ -46,25 +43,34 @@ WebNode::~WebNode() {
 // Web implementations.
 
 Web::Web(int n_children) {
-    printf("DEBUG:Web init started.\n");
-
     _n_children = n_children;
     _size = 0;
 
     _root = nullptr;
-    
-    printf("DEBUG:Web init ended.\n");
 }
 
 Web::~Web() {
-    printf("DEBUG:Web destr started.\n");
-
     if (_root != nullptr) {
         delete _root;
         _root = nullptr;
     }
-    
-    printf("DEBUG:Web destr ended.\n");
+}
+
+int Web::Insert(int v) {
+    // TODO(sky): implement with WebNode methods. First find.
+    return -1;
+}
+
+int Web::Remove(int v) {
+    // TODO(sky): implement with WebNode methods. First find.
+    return -1;
+}
+
+int Web::Find(int v) {
+    // TODO(sky): use this method.
+    _root->FirstChildWithAtMost(v);
+
+    return -1;
 }
 
 int Web::Size() { return _size; }
